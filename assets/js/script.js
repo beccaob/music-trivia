a5abeb8ca32055b5b5e721ae981aab82ae0446ca
beccaob <>
2023-09-09 22:04:57 +0000

    add question boilerplate and event listeners

---
+++ assets/js/script.js
@@ -0,0 +1,209 @@
+// divs & buttons from html 
+
+const restartBtn = document.getElementById('restart');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('previous');
+const submitBtn = document.getElementById('submit');
+const trueBtn = document.getElementById('true');
+const falseBtn = document.getElementById('false');
+const questionText = document.getElementById('question-text');
+const userScore = document.getElementById('user-score');
+
+// counter for current question 
+
+let currentQuestion = 0;
+
+// score counter 
+
+var currentScore = 0;
+
+// quiz questions 
+
+let questions = {
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+    {
+    question: "The Beatles' 'Yellow Submarine' was released as a single.",
+    answers: [
+        {option: 'True', answer:true}
+        {option: 'False', answer:false}
+    ]
+    },
+
+
+};
+
+// onclick event callers 
+
+restartBtn.addEventListener('click',restart);
+submitBtn.addEventListener('click',submit);
+prevBtn.addEventListener('click',previous);
+nextBtn.addEventListener('click',next)
+
+// begin quiz fenuction 
+
+function beginQuiz() {
+    currentQuestion = 0;
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        let ano=0;
+        if (questions = [currentQuestion].answers[ano].answer){
+            if (score<3){
+                score++;
+            }
+        }
+    }
+}
+
+
