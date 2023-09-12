15ed82d51b0bc74f3ffce1cedf8750cf9ddc873d
beccaob <>
2023-09-12 21:03:32 +0000

    get start button by element id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 // waits for html to load before loading javascript
 document.addEventListener("DOMContentLoaded", function () {
+
+    // Get DOM elements
+    const questionText = document.getElementById("question-text");
+    const trueBtn = document.getElementById("true");
+    const falseBtn = document.getElementById("false");
+    const userScoreElement = document.getElementById("user-score");
+    const endScoreElement = document.getElementById("end-score");
+    const previousBtn = document.getElementById("previous");
+    const nextBtn = document.getElementById("next");
+    const submitBtn = document.getElementById("submit");
+    const restartBtn = document.getElementById("restart");
+    const startBtn = document.getElementById("start");
+
+    let currentQuestion = 0;
+    let userScore = 0;
+
+
     //questions and answers
     const questions = [
         {
@@ -84,23 +101,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     ];
 
-    let currentQuestion = 0;
-    let userScore = 0; 
-
-    // Get DOM elements
-    const questionText = document.getElementById("question-text");
-    const trueBtn = document.getElementById("true");
-    const falseBtn = document.getElementById("false");
-    const userScoreElement = document.getElementById("user-score");
-    const endScoreElement = document.getElementById("end-score");
-    const previousBtn = document.getElementById("previous");
-    const nextBtn = document.getElementById("next");
-    const submitBtn = document.getElementById("submit");
-    const restartBtn = document.getElementById("restart");
-
     // randomises question order 
     function shuffleArray(array) {
-        for (let i= array.length - 1; i > 0; i--) {
+        for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
@@ -114,77 +117,75 @@ document.addEventListener("DOMContentLoaded", function () {
             const current = questions[currentQuestion];
             questionText.textContent = current.question;
         } else {
-            // quiz is over 
+            // quiz is over - all questions answered
             questionText.textContent = "You've finished the quiz!";
         };
 
-    // update user score 
-    function updateUserScore() {
-        userScoreElement.textContent = userScore;
-        endScoreElement.textContent = " / " + questions.length;
-    };
+        // update user score 
+        function updateUserScore() {
+            userScoreElement.textContent = userScore;
+            endScoreElement.textContent = " / " + questions.length;
+        };
 
-    // check users answer 
-    function checkAnswer(userAnswer) {
-        if (currentQuestion < questions.length) {
-            const current = questions[currentQuestion];
-            if (userAnswer === current.answer) {
-                userScore++;
+        // check users answer 
+        function checkAnswer(userAnswer) {
+            if (currentQuestion < questions.length) {
+                const current = questions[currentQuestion];
+                if (userAnswer === current.answer) {
+                    userScore++;
+                }
+                currentQuestion++;
+                displayCurrentQuestion();
+                updateUserScore();
             }
-            currentQuestion++;
+        };
+
+        // event listeners for true & false buttons 
+        trueBtn.addEventListener("click", () => checkAnswer(true));
+        falseBtn.addEventListener("click", () => checkAnswer(false));
+
+        // prev button event listener 
+        previousBtn.addEventListener("click", () => {
+            if (currentQuestion > 0) {
+                currentQuestion--;
+                displayCurrentQuestion();
+            }
+        });
+
+        // next button event listener 
+
+        nextBtn.addEventListener("click", () => {
+            if (currentQuestion < questions.length - 1) {
+                currentQuestion++;
+                displayCurrentQuestion();
+            }
+        });
+
+        // submit event listener 
+
+        submitBtn.addEventListener("click", () => {
+            let message = "";
+            if (userScore >= 15 && userScore <= 20) {
+                message = "Wow! You're amazing at this, well done!";
+            } else if (userScore >= 10 && userScore < 15) {
+                message = "You know your music trivia, congratulations!";
+            } else if (userScore <= 10) {
+                message = "Better luck next time!";
+            }
+            // Display the message
+            alert(message);
+        });
+
+        // restart event listener 
+        restartBtn.addEventListener("click", () => {
+            currentQuestion = 0;
+            userScore = 0;
+            shuffleArray(questions); // Shuffle questions again
             displayCurrentQuestion();
             updateUserScore();
-        }
-    };
+        });
 
-    // event listeners for true & false buttons 
-    trueBtn.addEventListener("click", () => checkAnswer(true));
-    falseBtn.addEventListener("click", () => checkAnswer(false)); 
-
-    // prev button event listener 
-    previousBtn.addEventListener("click", () => {
-        if (currentQuestion > 0) {
-            currentQuestion--;
-            displayCurrentQuestion();
-        }
-    });
-
-    // next button event listener 
-
-    nextBtn.addEventListener("click", () => {
-        if (currentQuestion < questions.length - 1) {
-            currentQuestion++;
-            displayCurrentQuestion();
-        }
-    });
-
-    // submit event listener 
-
-    submitBtn.addEventListener("click", () => {
-        let message = "";
-        if (userScore >= 15 && userScore <= 20) {
-            message = "Wow! You're amazing at this, well done!";
-        } else if (userScore >= 10 && userScore < 15) {
-            message = "You know your music trivia, congratulations!";
-        } else if (userScore <= 10) {
-            message = "Better luck next time!";
-        }
-        // Display the message
-        alert(message);
-    });
-
-    // restart event listener 
-    restartBtn.addEventListener("click", () => {
-        currentQuestion = 0;
-        userScore = 0;
-        shuffleArray(questions); // Shuffle questions again
+        // begin quiz 
         displayCurrentQuestion();
         updateUserScore();
     });
-
-    // begin quiz 
-    displayCurrentQuestion();
-    updateUserScore();
-});
-
-
