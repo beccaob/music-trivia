0be2cac46d36dfc1e8bd36a0f9d6ca63e3c1d6ec
beccaob <>
2023-09-25 19:38:15 +0000

    create username if/else statement that displays message when username is entered and error alert when its not

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const userScoreElement = document.getElementById("user-score");
     const endScoreElement = document.getElementById("end-score");
     const restartBtn = document.getElementById("restart");
+    const usernameInput = document.getElementById("username-input");
+    const startBtn = document.getElementById("start");
+    const submitBtn = document.getElementById("submit");
+
+    // hides start button before username is entered
+    startBtn.style.display = "none";
+
+    // start button event listener 
+
+    submitBtn.addEventListener("click", () => {
+        const username = usernameInput.value.trim(); // get entered username
+
+        // checks if username is provided 
+        if (username) {
+            const welcomeMessage = document.querySelector(".welcome h1"); // searches for h1 in welcome class and selects it
+            welcomeMessage.textContent = 'Hi, ${username}! Ready to test your knowledge?';
+            // hide input field & submit button
+            usernameInput.style.display = "none";
+            submitBtn.style.display = "none";
+            // shows start button 
+            startBtn.style.display = "";
+        } else {
+            // display error alert if no username is entered
+            alert("Please enter a username to start the game.");
+        }
+    });
+
 
     let currentQuestion = 0;
     let userScore = 0;
